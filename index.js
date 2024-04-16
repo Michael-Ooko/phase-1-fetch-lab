4ca1c925137a371aaa87b4ddfb83b89e6fce4da3
Michael <oliengo.michael@gmail.com>
2024-04-16 17:05:56 +0300

    dont grade will update

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Fetch data from the  API
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      // Convert the response to JSON
+      return response.json();
+    })
+    .then(data => {
+      // Call the renderBooks function and pass the JSON data
+      renderBooks(data);
+    })
+    .catch(error => {
+      // Handle any errors that occur during the fetch
+      console.error('Error fetching books:', error);
+    });
 }
 
 function renderBooks(books) {
@@ -15,3 +27,14 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+// // Call renderBooks() with the booksData JSON object
+const booksData = [
+  { name: "A Game of Thrones", author: "George R. R. Martin" },
+  { name: "A Clash of Kings", author: "George R. R. Martin" },
+  { name: "A Storm of Swords", author: "George R. R. Martin" },
+  { name: "A Feast for Crows", author: "George R. R. Martin" },
+  { name: "A Dance with Dragons", author: "George R. R. Martin" },
+  { name: "The Princess and the Queen"},
+  { name: "The Rogue Princ"}
+];
+renderBooks(data);
